4e894ad4bc7bea27ddc9ac22848c3f65c89e92e4
developerfunnel@gmail.com <ahanda205@gmail.com>
2022-03-30 19:47:49 +0530

    express router

--- fullstack/app.js
+++ fullstack/app.js
@@ -3,16 +3,29 @@ let app = express();
 let dotenv = require('dotenv');
 dotenv.config()
 let port = process.env.PORT || 8900;
+let categoryRouter = require('./src/router/categoryRouter')
+let productRouter = require('./src/router/productRouter');
+let morgan = require('morgan');
+let fs = require('fs');
+//app.use(morgan('combined'))
 
+app.use(morgan('short',{stream:fs.createWriteStream('./app.logs')}))
+
+// for template
+app.use(express.static(__dirname+'/public'))
+// html file path
+app.set('views','./src/views')
+//view engine
+app.set('view engine','ejs')
 
 ///default route
 app.get('/', function(req,res){
-    res.send("<h1>Welcome To Express</h1>")
+    //res.send("<h1>Welcome To Express</h1>")
+    res.render('first',{title:'First Ejs Code'})
 });
 
-app.get('/city', function(req,res){
-    res.send("<h1>Welcome To City</h1>")
-})
+app.use('/category', categoryRouter)
+app.use('/products', productRouter)
 
 app.listen(port,function(err){
     if(err) throw err;
