04fe9355082e00cdd4f7616ac40d67af24837049
aakashdeveloper <ahanda205@gmail.com>
2022-05-13 19:37:44 +0530

    final

--- fireapp/functions/index.js
+++ fireapp/functions/index.js
@@ -1,9 +1,28 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+const db = admin.firestore();
+
+exports.addCiti = functions.https.onRequest(async(req,res) => {
+    const citisRef = db.collection('marchnode');
+    await citisRef.doc('Delhi').set({
+        "name":"Delhi","Country":"India","Population":876566
+    })
+    res.send('Data Added')
+})
+
+exports.getMessage = functions.https.onRequest(async (req,res) => {
+    const citisRef = db.collection('marchnode');
+    const snapshot = await citisRef.get();
+    let out = []
+    snapshot.forEach(doc => {
+        out.push(doc.data())
+    })
+    res.send(out)
+})
